c1017dd415a8e5cd9aa0b0c51a4be59cf64ff7ce
Korenyk.A.N <korenyuk.a.n@mail.ru>
2025-05-05 11:07:27 +0500

    Version 0.0.27
    Промежуточный коммит

--- tests/test_mocked_creator.py
+++ tests/test_mocked_creator.py
@@ -1,14 +1,14 @@
 from unittest.mock import patch, MagicMock
 
-
-def test_mocked_creator():
+# Мок до импорта DatabaseMachineToolSource
+with patch("machine_tools.MachineToolsContainer.creator") as mocked_creator:
     fake_tool = MagicMock()
     fake_tool.model = "DMG CTX beta 2000"
+    mocked_creator.return_value.by_name.return_value = fake_tool
 
-    with patch("machine_tools.MachineToolsContainer.creator") as mocked_creator:
-        mocked_creator.return_value.by_name.return_value = fake_tool
+    # Импортировать после установки мока!
+    from design_of_mechanical_production.core.entities.machine_tool_source import DatabaseMachineToolSource
 
-        from design_of_mechanical_production.core.entities.machine_tool_source import DatabaseMachineToolSource
+    def test_mocked_creator():
         result = DatabaseMachineToolSource.get_machine_tool("DMG CTX beta 2000")
-
         assert result.model == "DMG CTX beta 2000"
