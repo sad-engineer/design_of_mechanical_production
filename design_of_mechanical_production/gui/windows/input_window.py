6ce54024f58535bef6d23707a5d548816c8de700
Korenyk.A.N <korenyuk.a.n@mail.ru>
2025-05-07 16:37:29 +0500

    Version 0.0.41.003
    Окно ввода начальных данных откорректировано, начало коррекции окна вывода результатов

--- design_of_mechanical_production/gui/windows/input_window.py
+++ design_of_mechanical_production/gui/windows/input_window.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------------------------------------------------
 """
-Модуль содержит основной класс окна ввода данных.
+Модуль содержит класс окна ввода данных, наследующий от шаблонного окна.
 """
 from kivy.clock import Clock
 from kivy.core.window import Window
@@ -10,53 +10,61 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
-from kivymd.app import MDApp
-from kivymd.uix.button import MDIconButton
 from kivymd.uix.label import MDLabel
+from kivymd.app import MDApp
+from kivy.graphics import Color, Rectangle
+from kivy.uix.widget import Widget
 
 from design_of_mechanical_production.core.services.workshop_creator import create_workshop_from_data
 from design_of_mechanical_production.gui.components.config import TableConfig
 from design_of_mechanical_production.gui.components.event_manager import TableEventManagerImpl
 from design_of_mechanical_production.gui.components.row_factory import BaseTableRowFactory
 from design_of_mechanical_production.gui.components.table import EditableTable
+from design_of_mechanical_production.gui.windows.template_window import TemplateWindow
 
 
-class InputWindow(FloatLayout):
+OPERATIONS = [
+    "Токарная",
+    "Токарная с ЧПУ",
+    "Сверлильная",
+    "Сверлильная с ЧПУ",
+    "Расточная",
+    "Расточная с ЧПУ",
+    "Фрезерная",
+    "Фрезерная с ЧПУ",
+    "Шлифовальная",
+    "Шлифовальная с ЧПУ",
+]
+
+
+class InputWindow(TemplateWindow):
     """
-    Окно ввода данных.
+    Окно ввода данных, наследующее от шаблонного окна.
 
     Attributes:
-        screen_manager: Менеджер экранов приложения.
         operations: Список доступных операций.
         table: Таблица с данными.
     """
 
-    def __init__(self, screen_manager=None, **kwargs):
-        super().__init__(**kwargs)
-        self.screen_manager = screen_manager
-        self._init_operations()
-        self._init_ui()
+    def __init__(self, screen_manager=None, debug_mode=False, **kwargs):
+        super().__init__(screen_manager=screen_manager, debug_mode=debug_mode, **kwargs)
+        # Инициализируем наш контент
+        self._init_content()
+        # Инициализируем кнопки
+        self._init_buttons()
 
-    def _init_operations(self):
-        """Инициализирует список доступных операций."""
-        self.operations = [
-            "Токарная",
-            "Токарная с ЧПУ",
-            "Сверлильная",
-            "Сверлильная с ЧПУ",
-            "Расточная",
-            "Расточная с ЧПУ",
-            "Фрезерная",
-            "Фрезерная с ЧПУ",
-            "Шлифовальная",
-            "Шлифовальная с ЧПУ",
-        ]
-
-    def _init_ui(self):
-        """Инициализирует пользовательский интерфейс."""
-        self._create_header()
-        self._create_columns()
-        self._create_buttons()
+    def _init_content(self):
+        """Инициализирует содержимое окна."""
+        columns = BoxLayout(
+            orientation='horizontal',
+            spacing=10,
+            size_hint_y=1,
+        )
+        left_col = self._create_left_column()
+        right_col = self._create_table_column()
+        columns.add_widget(left_col)
+        columns.add_widget(right_col)
+        self.content.add_widget(columns)
 
         # Программно изменяем размер окна для пересчета позиций
         def trigger_resize(dt):
@@ -67,137 +75,39 @@ class InputWindow(FloatLayout):
 
         Clock.schedule_once(trigger_resize, 0)
 
-    def _create_header(self):
-        """Создает заголовок окна."""
-        # Создаем однострочный контейнер для иконок
-        self.header = BoxLayout(
-            orientation='horizontal',
-            size_hint=(None, None),
-            width=40,
-            height=40,
-            padding=0,
-            spacing=5,
-            pos=(Window.width - 100, Window.height - 50),
-        )
-
-        # Добавляем иконки
-        self.theme_btn = MDIconButton(
-            icon="theme-light-dark", size_hint=(None, None), size=(40, 40), padding=0, on_release=self.toggle_theme
-        )
-        self.header.add_widget(self.theme_btn)
-
-        self.settings_btn = MDIconButton(
-            icon="cog", size_hint=(None, None), size=(40, 40), padding=0, on_release=self.open_settings
-        )
-        self.header.add_widget(self.settings_btn)
-
-        self.add_widget(self.header)
-
-        # Добавляем заголовок
-        self.label = MDLabel(
-            text='Ввод начальных условий',
-            size_hint=(1.25, None),
-            height=50,
-            pos=(0, Window.height - 50),
-            halign='center',
-            font_style='H5',
-        )
-        self.add_widget(self.label)
-
-        # Привязываем обновление позиции к изменению размера окна
-        Window.bind(size=self._update_header_position)
-
-    def _update_header_position(self, instance, size):
-        """Обновляет позицию header при изменении размера окна."""
-        width, height = size
-        self.header.pos = (width - 100, height - 50)
-        self.label.pos = (0, height - 50)
-
-    def _create_columns(self):
-        """Создает основные колонки интерфейса."""
-        # Основной контейнер с отступом снизу для кнопок
-        main_layout = BoxLayout(
-            orientation='vertical',
-            size_hint=(1, 1),
-            spacing=0,
-            padding=[10, -50, 10, 60],  # отступ снизу для кнопок
-        )
-
-        # Контейнер для колонок
-        columns = BoxLayout(
-            orientation='horizontal',
-            # size_hint=(1, 1),
-            spacing=20,
-            padding=0,
-        )
-
-        # Левый столбец
-        left_col = self._create_left_column()
-        columns.add_widget(left_col)
-
-        # Правая часть с таблицей
-        right_col = self._create_table_column()
-        columns.add_widget(right_col)
-
-        main_layout.add_widget(columns)
-        self.add_widget(main_layout)
-
-    def _create_buttons(self):
-        """Создает кнопки управления."""
-        # Контейнер для кнопок
-        buttons_box = BoxLayout(
-            orientation='horizontal',
-            size_hint=(0.5, None),
-            height=50,
-            spacing=5,
-            padding=[0, 0, 0, 5],
-            pos_hint={'center_x': 0.5, 'bottom': 0},
-        )  # Привязка к нижнему краю
-
-        # Кнопки
-        calc_btn = Button(text='Начать расчет', size_hint_x=0.5, height=40, on_release=self.save_data)
-        cancel_btn = Button(text='Отмена', size_hint_x=0.5, height=40, on_release=self.cancel)
-        buttons_box.add_widget(calc_btn)
-        buttons_box.add_widget(cancel_btn)
-        self.add_widget(buttons_box)
-
     def _create_left_column(self):
         """Создает левую колонку с настройками."""
-        left_col = FloatLayout(size_hint_x=None, width=275)
-
-        # Создаем контейнер для элементов
-        content = BoxLayout(
+        left_col = BoxLayout(
             orientation='vertical',
-            size_hint=(None, None),
             width=275,
-            height=200,
-            pos_hint={'x': 0, 'top': 1},
-            spacing=0,
-        )  # Позиционируем контент сверху
+            size_hint_x=None,
+            )
 
         # Название цеха
-        content.add_widget(MDLabel(text='Название цеха:', halign='left', size_hint_y=None, height=30))
+        left_col.add_widget(MDLabel(text='Название цеха:', halign='left', size_hint_y=None, height=30))
         self.name_input = TextInput(text='Механический цех №1', size_hint_y=None, halign='center', height=30)
-        content.add_widget(self.name_input)
+        left_col.add_widget(self.name_input)
 
         # Годовой объем производства
-        content.add_widget(
+        left_col.add_widget(
             MDLabel(text='Годовой объем производства (шт.):', halign='left', size_hint_y=None, height=30)
         )
         self.volume_input = TextInput(text='10000', size_hint_y=None, halign='center', height=30)
-        content.add_widget(self.volume_input)
+        left_col.add_widget(self.volume_input)
 
         # Масса детали
-        content.add_widget(MDLabel(text='Масса детали (кг):', halign='left', size_hint_y=None, height=30))
+        left_col.add_widget(MDLabel(text='Масса детали (кг):', halign='left', size_hint_y=None, height=30))
         self.mass_input = TextInput(text='112.8', size_hint_y=None, halign='center', height=30)
-        content.add_widget(self.mass_input)
+        left_col.add_widget(self.mass_input)
 
-        left_col.add_widget(content)
+        left_col.add_widget(Widget(size_hint_y=1))
+
+        left_col.bind(pos=self._update_left_col_debug, size=self._update_left_col_debug)
         return left_col
 
     def _create_table_column(self):
         """Создает правую колонку с таблицей."""
-        right_col = FloatLayout(size_hint_x=None, width=585)
+        right_col = BoxLayout(height=400, width=275, orientation='vertical')
 
         # Создаем конфигурацию таблицы
         table_config = TableConfig(
@@ -209,11 +119,11 @@ class InputWindow(FloatLayout):
                 ["015", "Токарная с ЧПУ", "5.65", "DMG CTX beta 2000"],
                 ["020", "Фрезерная с ЧПУ", "1.86", "DMU 50"],
             ],
-            operations=self.operations,
+            operations=OPERATIONS,
         )
 
         # Создаем фабрику строк
-        row_factory = BaseTableRowFactory(self.operations)
+        row_factory = BaseTableRowFactory(OPERATIONS)
 
         # Создаем таблицу (теперь она сама содержит прокрутку и рамку)
         self.table = EditableTable(
@@ -228,7 +138,45 @@ class InputWindow(FloatLayout):
         self.table.event_manager = TableEventManagerImpl(self.table)
         right_col.add_widget(self.table)
 
+        right_col.bind(pos=self._update_right_col_debug, size=self._update_right_col_debug)
         return right_col
+
+    def _update_left_col_debug(self, instance, value):
+        if self.debug_mode:
+            instance.canvas.before.clear()
+            with instance.canvas.before:
+                Color(0, 0, 0, 0.3)     # Черный с прозрачностью
+                Rectangle(pos=instance.pos, size=instance.size)
+
+    def _update_right_col_debug(self, instance, value):
+        if self.debug_mode:
+            instance.canvas.before.clear()
+            with instance.canvas.before:
+                Color(0, 0, 1, 0.3)  # Синий с прозрачностью
+                Rectangle(pos=instance.pos, size=instance.size)
+
+    def _init_buttons(self):
+        """Инициализирует кнопки управления."""
+        # Очищаем существующие кнопки
+        self.buttons_box.clear_widgets()
+        
+        # Создаем новые кнопки
+        calc_btn = Button(
+            text='Начать расчет',
+            size_hint=(None, 1),
+            width=self.max_button_width,
+            on_release=self.save_data
+        )
+        cancel_btn = Button(
+            text='Отмена',
+            size_hint=(None, 1),
+            width=self.max_button_width,
+            on_release=self.cancel
+        )
+        
+        # Добавляем кнопки в контейнер
+        self.buttons_box.add_widget(calc_btn)
+        self.buttons_box.add_widget(cancel_btn)
 
     def save_data(self, instance):
         """Сохраняет введенные данные и переходит к результатам расчета."""
@@ -256,33 +204,6 @@ class InputWindow(FloatLayout):
         except ValueError as e:
             print("Ошибка ввода данных:", e)
 
-    @staticmethod
-    def cancel(instance):
-        """
-        Отменяет ввод данных и завершает работу приложения.
-
-        Args:
-            instance: Экземпляр кнопки
-        """
-        # Завершаем работу приложения
-        MDApp.get_running_app().stop()
-
-    @staticmethod
-    def toggle_theme(instance):
-        """Переключает тему приложения."""
-        app = MDApp.get_running_app()
-        if app.theme_cls.theme_style == "Light":
-            app.theme_cls.theme_style = "Dark"
-        else:
-            app.theme_cls.theme_style = "Light"
-
-    def open_settings(self, instance):
-        """Открывает окно настроек."""
-        if self.screen_manager:
-            self.screen_manager.current = 'settings'
-        else:
-            print("screen_manager не передан!")
-
     def get_table_data(self):
         """Возвращает данные из таблицы."""
         return self.table.get_data()
@@ -290,3 +211,17 @@ class InputWindow(FloatLayout):
     def set_table_data(self, new_data):
         """Устанавливает новые данные в таблицу."""
         self.table.set_data(new_data)
+
+
+if __name__ == '__main__':
+    class TestApp(MDApp):
+        """Тестовое приложение для отладки окна."""
+
+        def build(self):
+            """Создает и возвращает главное окно приложения."""
+            Window.minimum_width = 910
+            Window.minimum_height = 500
+            window = InputWindow(debug_mode=True)
+            return window
+
+    TestApp().run()
